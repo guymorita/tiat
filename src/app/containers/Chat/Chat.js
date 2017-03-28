1ba72c0e1e23f0c354269b204a72236d014b44f7
Guy Morita <guymorita@gmail.com>
2017-03-28 15:30:27 +0700

    break out colors

--- src/app/containers/Chat/Chat.js
+++ src/app/containers/Chat/Chat.js
@@ -13,36 +13,9 @@ import NavigationBar from 'react-native-navbar'
 import _ from 'lodash'
 import { Bubble, GiftedChat } from 'react-native-gifted-chat'
 
-user1 = {
-  _id: 1,
-  avatar: 'https://s-media-cache-ak0.pinimg.com/736x/47/5e/0f/475e0f1362a7526c16d604f5dac47b86.jpg'
-}
+import { getBackgroundStyle, getBackgroundColor } from '../../lib/colors'
 
-user2 = {
-  _id: 2,
-  avatar: 'https://s-media-cache-ak0.pinimg.com/736x/47/5e/0f/475e0f1362a7526c16d604f5dac47b86.jpg'
-}
-
-const TINDER_COLOR = '#ED7C61'
-const IMESSAGE_COLOR = '#2D75FA'
-
-const getBackgroundStyle = (platform) => {
-  switch(platform) {
-    case 'tinder':
-      return styles.backgroundTinder
-    case 'iMessage':
-      return styles.backgroundiMessage
-  }
-}
-
-const getBackgroundColor = (platform) => {
-  switch(platform) {
-    case 'tinder':
-      return TINDER_COLOR
-    case 'iMessage':
-      return IMESSAGE_COLOR
-  }
-}
+const TEXT_COLOR_LIGHT = 'white'
 
 class Chat extends React.Component {
   constructor(props) {
@@ -63,6 +36,7 @@ class Chat extends React.Component {
       platform = firstThread.platform
     }
 
+    // FIX get rid of all state except messages
     this.state = {
       messages: [],
       currentLineRender: 0,
@@ -89,6 +63,8 @@ class Chat extends React.Component {
     const { navigator } = this.props
     navigator.pop()
   }
+
+  // FIX remove all functions except button handlers and render functions
 
   nextStep() {
     const { currentChat, threads } = this.state
@@ -208,17 +184,13 @@ class Chat extends React.Component {
     const firstBgColor = getBackgroundColor(currentChat.platform)
     const { currentMessage } = props
     const secondBgColor = currentMessage.cha_id === 2 ? '#D0E2F4': '#F0F0F0'
-    // console.log('props', props)
-    // #F0F0F0
-    // #DCDCDC
-    // #D0E2F4
 
     return (
       <Bubble
         {...props}
         textStyle={{
           right: {
-          color: 'white',
+          color: TEXT_COLOR_LIGHT,
         }
         }}
         wrapperStyle={{
@@ -268,10 +240,10 @@ class Chat extends React.Component {
     const backgroundStyle = getBackgroundStyle(currentChat.platform)
 
     return (
-      <TouchableOpacity key={option.dec_id} style={styles.bottomBubbleTouch} onPress={this._onOptionPress.bind(this, option)}>
-        <View  style={[styles.bottomBubble, backgroundStyle]}>
+      <TouchableOpacity key={option.dec_id} style={styles.optionBubbleTouch} onPress={this._onOptionPress.bind(this, option)}>
+        <View  style={[styles.bubbleBase, styles.optionBubble, backgroundStyle]}>
           <Text
-            style={styles.optionBubble}
+            style={styles.optionBubbleText}
             numberOfLines={2}>
             {option.text}
           </Text>
@@ -288,7 +260,7 @@ class Chat extends React.Component {
     return (
       <View style={styles.inputToolbar}>
         <TouchableOpacity onPress={this._onNextPress.bind(this)}>
-          <View style={[styles.nextBubble, backgroundStyle]}>
+          <View style={[styles.bubbleBase, styles.nextBubble, backgroundStyle]}>
             <Text
               style={styles.nextBubbleText}>
               Next
@@ -343,60 +315,42 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
-  composerContainer: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    flex: 1,
-    bottom: 0
-  },
   inputToolbar: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center'
   },
-  nextBubble: {
+  bubbleBase: {
+    flex: 1,
     borderRadius: 15,
     borderBottomRightRadius: 0,
-    marginLeft: 50,
-    marginRight: 50,
     marginTop: 10,
-    paddingLeft: 50,
-    paddingRight: 50,
     minHeight: 20,
     justifyContent: 'center',
+    height: 70
+  },
+  nextBubble: {
+    marginLeft: 50,
+    marginRight: 50,
+    paddingLeft: 50,
+    paddingRight: 50,
     alignItems: 'center',
-    height: 70,
-    flex: 1
-
   },
   nextBubbleText: {
     fontSize: 16,
-    color: 'white'
+    color: TEXT_COLOR_LIGHT
   },
-  bottomBubbleTouch: {
-    flex: 1
-  },
-  bottomBubble: {
-    borderRadius: 15,
-    borderBottomRightRadius: 0,
-    marginLeft: 10,
-    marginRight: 10,
-    marginTop: 10,
-    paddingLeft: 10,
-    paddingRight: 10,
-    minHeight: 20,
-    justifyContent: 'center',
-    height: 80,
+  optionBubbleTouch: {
     flex: 1
   },
   optionBubble: {
-    color: 'white'
+    marginLeft: 10,
+    marginRight: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
   },
-  backgroundTinder: {
-    backgroundColor: TINDER_COLOR,
-  },
-  backgroundiMessage: {
-    backgroundColor: IMESSAGE_COLOR
+  optionBubbleText: {
+    color: TEXT_COLOR_LIGHT
   }
 })
 
---
+++ src/app/lib/colors.js
@@ -0,0 +1,34 @@
+
+import {
+  StyleSheet
+} from 'react-native'
+
+const TINDER_COLOR = '#ED7C61'
+const IMESSAGE_COLOR = '#2D75FA'
+
+export const getBackgroundStyle = (platform) => {
+  switch(platform) {
+    case 'tinder':
+      return styles.backgroundTinder
+    case 'iMessage':
+      return styles.backgroundiMessage
+  }
+}
+
+export const getBackgroundColor = (platform) => {
+  switch(platform) {
+    case 'tinder':
+      return TINDER_COLOR
+    case 'iMessage':
+      return IMESSAGE_COLOR
+  }
+}
+
+const styles = StyleSheet.create({
+  backgroundTinder: {
+    backgroundColor: TINDER_COLOR,
+  },
+  backgroundiMessage: {
+    backgroundColor: IMESSAGE_COLOR
+  }
+})
