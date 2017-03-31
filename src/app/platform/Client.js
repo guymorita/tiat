7f034a9c862daea3e5f2a242d224c580757a5fc8
Guy Morita <guymorita@gmail.com>
2017-04-01 01:32:48 +0700

    add redux-persist

--- src/app/platform/Client.js
+++ src/app/platform/Client.js
@@ -1,18 +1,31 @@
 
-import { createStore, applyMiddleware } from 'redux'
+import { AsyncStorage } from 'react-native'
+
+import { compose, createStore, applyMiddleware } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger'
+import { persistStore, autoRehydrate } from 'redux-persist'
 import rootReducer from '../reducers'
 
 const loggerMiddleware = createLogger()
 
 export default function configureStore(preloadedState) {
-  return createStore(
+  const store = createStore(
     rootReducer,
     preloadedState,
-    applyMiddleware(
-      thunkMiddleware,
-      loggerMiddleware
+    compose(
+      applyMiddleware(
+        thunkMiddleware,
+        loggerMiddleware
+      ),
+      autoRehydrate({log: true})
     )
   )
+  persistStore(store, {
+    storage: AsyncStorage,
+    blacklist: ['matches', 'characters']
+  }, () => {
+    console.log('rehydration complete')
+  })
+  return store
 }
