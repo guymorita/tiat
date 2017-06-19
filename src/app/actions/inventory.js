cfe6dad45e083287ed8ed26f988ebc9a91fe2926
Guy Morita <guymorita@gmail.com>
2017-06-19 00:55:28 -0700

    fix skip naming bug

--- src/app/actions/inventory.js
+++ src/app/actions/inventory.js
@@ -24,7 +24,7 @@ export function invJumpsSubtract(quantity) {
 export function inventoryChange(prod) {
   return (dispatch) => {
     const { quantity } = prod
-    if (prod.type === JUMP) {
+    if (prod.type === SKIP) {
       dispatch(invJumpsAdd(quantity))
     }
   }
